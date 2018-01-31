02ebd3ce79af04f033c0b970bcb6f03f6bd68d41
Jorg <dildovaper@yahoo.com>
2018-01-31 19:49:08 +0200

    Temperature Converter

---
+++ 4.TemperatureConverter/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("Would you like to convert Fahrenheit or Celcium?\n---- Type F or C to choose! ----\n");
+
+    char selection;
+
+    scanf("%c",&selection);
+
+    if (selection != 'c' && selection != 'C' && selection != "F" && selection != 'f') //Checks if the selection was valid
+    {
+        printf("Invalid selection!\n");
+        return 0;
+    }
+    else
+    {
+
+        float temp;
+
+        printf("Please enter the temperature you'd like to convert!\n");
+        scanf("%f",&temp);
+
+            if(selection=='F' || selection== 'f')
+            {
+                printf("%.2fF = %.2f °C",temp,(5.0/9.0)*temp);      //Converts Fahrenheit to Celcius and prints out the value!
+            }
+            else if(selection=='C' || selection== 'c')
+            {
+        printf("%.2f°C = %.2fF",temp,(9.0/5.0)*temp);       //Converts Celcius to Fahrenheit and prints out the value!
+            }
+    }
+}
+
